30b0e92065085c9d70f09abe85f7270a7c1ff367
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-04-12 23:23:31 -0700

    feat: add account entity

--- src/domain/entries/account.entity.ts
+++ src/domain/entries/account.entity.ts
@@ -1,1 +1,23 @@
+import { MoneyEntity } from './money.entity';
+
 export type AccountId = number;
+
+class AccountEntity {
+  constructor(
+    private readonly _id: AccountId,
+    private readonly _name: string,
+    private readonly _balance: MoneyEntity,
+  ) {}
+
+  get id(): AccountId {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get balance(): MoneyEntity {
+    return this._balance;
+  }
+}
