fbc80b4a3fd4c80a5ca6ef5702c20e3557997dd5
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-04-14 22:38:04 -0700

    feat: add account service

---
+++ src/domain/services/get-account-balance.service.ts
@@ -0,0 +1,10 @@
+import { AccountId } from '../entries/account.entity';
+import { LoadAccountPort } from '../ports/out/load-account.port';
+
+export class GetAccountBalanceService {
+  constructor(private readonly _loadAccountPort: LoadAccountPort) {}
+
+  getAccountBalance(accountId: AccountId) {
+    return this._loadAccountPort.loadAccount(accountId);
+  }
+}
