49c6ed58f4cc3c14326be0e8f409aff70966fe7a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-04-15 11:09:23 -0700

    fix: get account balance

--- src/domain/services/get-account-balance.service.ts
+++ src/domain/services/get-account-balance.service.ts
@@ -1,10 +1,13 @@
 import { AccountId } from '../entities/account.entity';
+import { MoneyEntity } from '../entities/money.entity';
+import { GetAccountBalanceQuery } from '../ports/in/get-account-balance.query';
 import { LoadAccountPort } from '../ports/out/load-account.port';
 
-export class GetAccountBalanceService {
+export class GetAccountBalanceService implements GetAccountBalanceQuery {
   constructor(private readonly _loadAccountPort: LoadAccountPort) {}
 
-  getAccountBalance(accountId: AccountId) {
-    return this._loadAccountPort.loadAccount(accountId);
+  async getAccountBalance(accountId: AccountId): Promise<MoneyEntity> {
+    const account = await this._loadAccountPort.loadAccount(accountId);
+    return account.calculateBalance();
   }
 }
