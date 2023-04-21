718423773ec519dd8f26289b5f7e64641c7902d4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-04-20 23:31:13 -0700

    fix: send money port

--- src/domain/ports/in/send-money.use-case.ts
+++ src/domain/ports/in/send-money.use-case.ts
@@ -3,5 +3,5 @@ import { SendMoneyCommand } from './send-money.command';
 export const SendMoneyUseCaseSymbol = Symbol('SendMoneyUseCase');
 
 export interface SendMoneyUseCase {
-  sendMoney(command: SendMoneyCommand): boolean;
+  sendMoney(command: SendMoneyCommand): Promise<boolean>;
 }
